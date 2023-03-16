39ebb3cf6c64406b1b7b2895c6e80d501ecbbe57
muham <muhambriana.31@gmail.com>
2023-03-16 22:30:33 +0700

    Initial commit

---
+++ core/src/main/java/com/muhamapps/filmcatalogueapp1/core/di/CoreModule.kt
@@ -0,0 +1,71 @@
+package com.muhamapps.filmcatalogueapp1.core.di
+
+import androidx.room.Room
+import com.muhamapps.filmcatalogueapp1.core.data.FilmRepository
+import com.muhamapps.filmcatalogueapp1.core.data.source.local.LocalDataSource
+import com.muhamapps.filmcatalogueapp1.core.data.source.local.room.FilmDatabase
+import com.muhamapps.filmcatalogueapp1.core.data.source.remote.RemoteDataSource
+import com.muhamapps.filmcatalogueapp1.core.data.source.remote.network.ApiService
+import com.muhamapps.filmcatalogueapp1.core.domain.repository.IFilmRepository
+import com.muhamapps.filmcatalogueapp1.core.utils.AppExecutors
+import com.muhamapps.filmcatalogueapp1.core.utils.NetworkInfo.BASE_URL
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
+import okhttp3.CertificatePinner
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.android.ext.koin.androidContext
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+val databaseModule = module {
+    factory { get<FilmDatabase>().filmDao() }
+    single {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("dicoding".toCharArray())
+        val factory = SupportFactory(passphrase)
+        Room.databaseBuilder(
+            androidContext(),
+            FilmDatabase::class.java, "Film.db"
+        ).fallbackToDestructiveMigration().openHelperFactory(factory).build()
+    }
+}
+
+val networkModule = module {
+    single {
+        val hostname = "api.themoviedb.org"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/NPIMWkzcNG/MyZsVExrC6tdy5LTZzeeKg2UlnGG55UY=")
+            .add(hostname, "sha256/DxH4tt40L+eduF6szpY6TONlxhZhBd+pJ9wbHlQ2fuw=")
+            .add(hostname, "sha256/++MBgDH5WGvL9Bcn5Be30cRcL0f5O+NyoXuWtQdX1aI=")
+            .build()
+        OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .connectTimeout(120, TimeUnit.SECONDS)
+            .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
+            .build()
+    }
+    single {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(get())
+            .build()
+        retrofit.create(ApiService::class.java)
+    }
+}
+
+val repositoryModule = module {
+    single { LocalDataSource(get()) }
+    single { RemoteDataSource(get()) }
+    factory { AppExecutors() }
+    single<IFilmRepository> {
+        FilmRepository(
+            get(),
+            get(),
+            get()
+        )
+    }
+}
