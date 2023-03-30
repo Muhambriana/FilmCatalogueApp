c1d302473b0809334db3dc91b1459c2a9df6442f
muham <muhambriana.31@gmail.com>
2023-03-30 09:29:02 +0700

    add adMob library and implement it

--- app/src/main/java/com/muhamapps/filmcatalogueapp1/home/HomeActivity.kt
+++ app/src/main/java/com/muhamapps/filmcatalogueapp1/home/HomeActivity.kt
@@ -1,15 +1,22 @@
 package com.muhamapps.filmcatalogueapp1.home
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.content.res.Configuration
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import androidx.core.app.ShareCompat
 import androidx.recyclerview.widget.GridLayoutManager
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 import com.muhamapps.filmcatalogueapp1.R
 import com.muhamapps.filmcatalogueapp1.core.data.Resource
 import com.muhamapps.filmcatalogueapp1.core.domain.model.Film
@@ -23,6 +30,7 @@ class HomeActivity : AppCompatActivity(), FilmShareCallback {
 
     private val homeViewModel: HomeViewModel by viewModel()
 
+    private var mInterstitialAd: InterstitialAd? = null
     private var _binding: ActivityHomeBinding? = null
     private val binding get() = _binding
 
@@ -33,7 +41,33 @@ class HomeActivity : AppCompatActivity(), FilmShareCallback {
         supportActionBar?.setLogo(R.drawable.actionbar_icon)
 
 
+        admobInit()
         getFilmData()
+    }
+
+    private fun admobInit() {
+        MobileAds.initialize(this)
+
+        val adRequest = AdRequest.Builder().build()
+        InterstitialAd.load(this,"ca-app-pub-3940256099942544/1033173712", adRequest, object : InterstitialAdLoadCallback() {
+            override fun onAdFailedToLoad(adError: LoadAdError) {
+                Log.d(TAG, adError.toString())
+                mInterstitialAd = null
+            }
+
+            override fun onAdLoaded(interstitialAd: InterstitialAd) {
+                Log.d(TAG, "Ad was loaded.")
+                mInterstitialAd = interstitialAd
+            }
+        })
+
+        binding?.myButton?.setOnClickListener {
+            if (mInterstitialAd != null) {
+                mInterstitialAd?.show(this)
+            } else {
+                Log.d("TAG", "The interstitial wasn't loaded yet.")
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
